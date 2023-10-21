f29e29f5bab34d598fc2d241117b0eb9e12f2ce7
unknown <Valentin@nbgrimmenvy.aes.th-owl.de>
2023-10-21 13:59:27 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, request, make_response, jsonify
+# from StandardBot import StandardBot
+from InteractiveBot import InteractiveBot
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from waitress import serve
+
+# import warnings
+# warnings.filterwarnings("ignore")
+
+# Prepare environment variables
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CURRENT_OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+os.environ['OPENAI_API_KEY'] = CURRENT_OPENAI_API_KEY
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_VERSION"] = "2023-05-15"
+os.environ["OPENAI_API_BASE"] = "https://nextvisionopenai.openai.azure.com"
+os.environ["OPENAI_DEPLOYMENT_NAME"] = "GPT35Turbo"
+os.environ["OPENAI_MODEL_NAME"] = "gpt-3.5-turbo"
+
+# init GPT Objects
+def create_app(config=None):
+    app = Flask(__name__)
+
+    standard_bot = InteractiveBot()
+
+    limiter = Limiter(
+        app=app,
+        key_func=get_remote_address,  # This will use the user's IP to track the requests
+        default_limits=["20 per minute"]
+    )
+
+    @app.route("/api/test", methods=["GET", "OPTIONS"])
+    # @limiter.limit("1 per minute")
+    def test_api():
+        
+        if request.method == "OPTIONS":
+            return _build_cors_preflight_response()
+        elif request.method == "GET": 
+            gpt_response = standard_bot.test_call()
+            response = make_response(gpt_response)
+            return _corsify_actual_response(response)
+        else:
+            raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+        
+    @app.route("/api/recommend", methods=["POST", "OPTIONS"])
+    def recommend():
+        
+        if request.method == "OPTIONS":
+            return _build_cors_preflight_response()
+        elif request.method == "POST": 
+            request.get_json(force=True)
+            request_json = request.json
+            print(request_json)
+            print(request_json["setup"])
+            gpt_response = standard_bot.handle_suggestion_request(request_json["setup"], request_json["history"])
+            response = make_response(gpt_response)
+            return _corsify_actual_response(response)
+        else:
+            raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+
+
+    def _build_cors_preflight_response():
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add('Access-Control-Allow-Headers', "*")
+        response.headers.add('Access-Control-Allow-Methods', "*")
+        return response
+
+    def _corsify_actual_response(response):
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
+    # Error handler for rate limiting
+    @app.errorhandler(429)
+    def ratelimit_error(e):
+        return jsonify(success=False, message="ratelimit exceeded", error=str(e)), 429
+    
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    serve(app, host='0.0.0.0', port=5000)
---
+++ defined_texts.py
@@ -0,0 +1,10 @@
+intro = '''Du schreibst eine interaktive Geschichte, in der ein Szenario von privat angestellten Hausangestellten mit folgenden Eigenschaften beschrieben wird: \
+            - Maximal zwei Charaktere interagieren miteinander\
+            - Das Szenario spielt im Kontext der folgenden Tätigkeit: {workArea}
+            - Der/die Arbeitgeber*in heißt: {employer}. Über diese Person ist folgendes bekannt: {employerInfo}
+            - Der/die Hausangestellte heißt: {employee}. Über diese Person ist folgendes bekannt: {employeeInfo}
+            - Die gleiche Person kann mehrere Textabschnitte hintereinander sagen\
+            - Die Geschichte dient zur Weiterbildung der Hausangestellten\
+            - Es soll eine runde Geschichte entstehen, die eingeleitet wird und hin zu einem Entscheidungsszenario (Auswahl zwischen drei Möglichkeiten) für die Hausangestellte Person führt\
+            - Nach der Entscheidung wird die Geschichte in den Schlussteil geführt\
+'''
